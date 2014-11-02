0171ec91ce48a6b850c22fe405b5d25653e6fee9
DF4ze <df4ze@free.fr>
2014-11-02 14:28:12 +0100

    Ajout d'un fieldSet autour du DirectionPanel ... ... Il va falloir refaire les images : - transparence - même taille du cercle - Bonne dimension
    Du coup il n'y aura peut-etre plus besoin du emptyPanel pour que le
    fiedset soit bien positionné.

--- src/vues/campanels/ControlPanel.java
+++ src/vues/campanels/ControlPanel.java
@@ -2,6 +2,7 @@ package vues.campanels;
 
 import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JPanel;
 
@@ -23,11 +24,15 @@ public class ControlPanel extends JPanel {
 	public ControlPanel( CtrlCat oModel ) {
 		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 		
+		JPanel emptyP = new JPanel();
+		emptyP.setBorder(BorderFactory.createTitledBorder("Direction"));
 		BgPanel directionBgPanel = new BgPanel( "images/EMaps-Center-Direction-icon.png" );
+		emptyP.add(directionBgPanel);
 		
 		directionPanel = new DirectionPanel(oModel, directionBgPanel);
 		directionBgPanel.add(directionPanel);
-		add( directionBgPanel );
+		
+		add( emptyP );
 		
 		tourPanel = new TourellePanel(oModel);
 		add(tourPanel);
